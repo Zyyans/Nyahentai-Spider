aa0cf7adfab559ad6499127f88383e18180ff3f8
Zyyans <1980797568@qq.com>
2019-05-24 12:55:34 +0800

    完善 List 类

---
+++ class_Page.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+from requests import get
+
+
+class Page():
+
+    def soup_init(self, url):
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) App\
+        leWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.108 Safari/537.36'}
+        response = get(url, headers=headers)
+        if response.ok:
+            return BeautifulSoup(response.text, 'lxml')
+        else:
+            return None

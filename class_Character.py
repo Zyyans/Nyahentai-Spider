ba23643ea3a3e55ec6d39a86593d57a0eff0ad8e
Zyyans <1980797568@qq.com>
2019-05-28 11:58:12 +0800

    暂时停止重写

--- class_Character.py
+++ class_Character.py
@@ -7,6 +7,18 @@ class Character(Page):
         super().__init__()
         self.name = name
         self.url = self.mainurl + 'character/' + name + '/'
+        self.path = 'nyahentai/' + self.name + '/'
         self.list = {}
-    
-    # 有点累. 先鸽了.
+
+    def network_init(self, chinese=False):
+        if chinese:
+            self.url = self.url + 'chinese/'
+        self.soup = self.soup_init(self.url)
+        if self.soup:
+            print('\n[Info] 角色 ' + self.name + ' 的资源列表初始化完毕\n')
+        else:
+            if chinese == True:
+                print('>> 该角色无中文资源')
+            print('\n[Info] 角色 ' + self.name + ' 的资源列表初始化失败\n')
+
+# 迟迟没有进展 对其他需要的方法没啥思路 先鸽了
